81998d8cfc36b1a60354a467833f4c503a40bccd
whil <miltonivan117@gmail.com>
2023-04-12 16:29:59 -0600

    update vite project jotai

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { AtomText, AtomWrapper } from "lucy-nxtjs"
+
+function App() {
+
+  return (
+    <AtomWrapper>
+      <AtomText>hola saasdfsdafsdaff            </AtomText>
+
+
+
+
+
+
+
+
+    </AtomWrapper>
+  )
+}
+
+export default App
